4b4da044c545f0d613f0a7c0fee4dc5101a722ed
gayatri <113498352+gay-tr-i@users.noreply.github.com>
2022-10-28 09:31:13 +0530

    Add files via upload

---
+++ arr_push.js
@@ -0,0 +1,12 @@
+const arr=[]
+arr.push("sakshi")
+arr.push("gayatri")
+console.log(arr)
+
+arr.push("meghna")
+arr.push("gayatri")
+console.log(arr)
+
+arr.push("prachi")
+arr.push("neha")
+console.log(arr)
